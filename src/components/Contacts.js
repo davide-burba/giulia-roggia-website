6955fd36236f317bde596157f1294755ae7570a5
Davide <davide.burba@hotmail.com>
2023-03-30 23:02:15 +0200

    Include contacts list in paragraph

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -9,11 +9,11 @@ function Contacts() {
         <p>
           If you're interested in working with me or would like to see more of my 
           work, feel free to reach out!
+          <ul>
+            <li>Email: <a href="mailto: giuliaroggia@outlook.it"> giuliaroggia@outlook.it </a></li>
+            <li>Instagram: <a href="https://www.instagram.com/giulia_roggia__/"> giulia_roggia__ </a></li>
+          </ul>
         </p>
-        <ul>
-          <li>Email: <a href="mailto: giuliaroggia@outlook.it"> giuliaroggia@outlook.it </a></li>
-          <li>Instagram: <a href="https://www.instagram.com/giulia_roggia__/"> giulia_roggia__ </a></li>
-        </ul>
       </div>
     </section>
   );
